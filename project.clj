9da45503176accf063291381111c2ce13989a7aa
Gavin Lam <me@gavin.hk>
2018-06-01 00:55:31 +0800

    Implemented extract-tables-test for service using clj-http client

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
   :resource-paths ["src/main/resources"]
-  :profiles {:dev {:dependencies [[commons-io/commons-io "2.6"]
+  :profiles {:dev {:dependencies [[clj-http "3.9.0"]
+                                  [commons-io/commons-io "2.6"]
                                   [io.pedestal/pedestal.service-tools "0.5.3"]]
                    :resource-paths ["src/test/resources"]}}
   :main hk.gavin.tabula-api.server)
--- src/test/clj/hk/gavin/tabula_api/service_test.clj
+++ src/test/clj/hk/gavin/tabula_api/service_test.clj
@@ -1,18 +1,33 @@
 (ns hk.gavin.tabula-api.service-test
   (:require [clojure.test :refer :all]
-            [io.pedestal.test :refer :all]
-            [io.pedestal.http :as http]
-            [hk.gavin.tabula-api.service :as service]))
+            [clojure.java.io :as io]
+            [clj-http.client :as client]
+            [hk.gavin.tabula-api.server :as server])
+  (:import (org.apache.commons.io FileUtils)))
 
-(def service
-  (::http/service-fn (http/create-servlet service/service)))
+(def base-req
+  {:scheme :http
+   :server-name "localhost"
+   :server-port 8080})
 
-(deftest hello-world-test
-  (is (=
-       (:body (response-for service :get "/hello"))
-       "hello world.\n")))
+(def sample-form
+  [{:name "area"   :content "%0,0,100,50"}
+   {:name "area"   :content "%0,50,100,100"}
+   {:name "format" :content "CSV"}
+   {:name "pages"  :content "1"}
+   {:name "stream" :content "yes"}
+   {:name "file"   :content (io/file (io/resource "multi-column.pdf"))}])
 
-(deftest hello-world-with-name-test
-  (is (=
-       (:body (response-for service :get "/hello?name=gavin"))
-       "hello gavin.\n")))
+(defn request
+  [req]
+  (client/request (merge base-req req)))
+
+(deftest extract-tables-test
+  (let [resp (request {:uri "/extract_tables"
+                       :method :post
+                       :multipart sample-form})
+        expect-csv (io/file (io/resource "multi-column.csv"))
+        output-csv (java.io.File/createTempFile "extract-tables-test" ".csv")]
+    (is (= (get resp :status) 200))
+    (FileUtils/writeStringToFile output-csv (get resp :body) nil)
+    (is (FileUtils/contentEqualsIgnoreEOL expect-csv output-csv nil))))
