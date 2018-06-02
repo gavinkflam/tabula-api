18ba142d84d76c42e547705e2d02ef29ffe76725
Gavin Lam <me@gavin.hk>
2018-06-03 00:42:13 +0800

    Make use of defonce and delay for servers instead of def in def

--- src/main/clj/hk/gavin/tabula_api/server.clj
+++ src/main/clj/hk/gavin/tabula_api/server.clj
@@ -3,31 +3,35 @@
   (:require [io.pedestal.http :as http]
             [hk.gavin.tabula-api.service :as service]))
 
+(defonce prod-serv
+  (delay (http/create-server service/service)))
+
+(defonce dev-serv
+  (delay
+   (-> service/service ;; start with production configuration
+       (merge {:env :dev
+                ;; do not block thread that starts web server
+               ::http/join? false
+                ;; Routes can be a function that resolve routes,
+                ;;  we can use this to set the routes to be reloadable
+               ::http/routes #(deref #'service/routes)
+                ;; all origins are allowed in dev mode
+               ::http/allowed-origins {:creds true
+                                       :allowed-origins (constantly true)}})
+        ;; Wire up interceptor chains
+       http/default-interceptors
+       http/dev-interceptors
+       http/create-server)))
+
 (defn run-prod
   "Create the production server if not yet created, and start the server."
   []
-  (defonce prod-serv (http/create-server service/service))
-  (http/start prod-serv))
+  (http/start @prod-serv))
 
 (defn run-dev
   "Create the development server if not yet created, and start the server."
   []
-  (defonce dev-serv
-    (-> service/service ;; start with production configuration
-        (merge {:env :dev
-                ;; do not block thread that starts web server
-                ::http/join? false
-                ;; Routes can be a function that resolve routes,
-                ;;  we can use this to set the routes to be reloadable
-                ::http/routes #(deref #'service/routes)
-                ;; all origins are allowed in dev mode
-                ::http/allowed-origins {:creds true
-                                        :allowed-origins (constantly true)}})
-        ;; Wire up interceptor chains
-        http/default-interceptors
-        http/dev-interceptors
-        http/create-server))
-  (http/start dev-serv))
+  (http/start @dev-serv))
 
 (defn -main
   "The entry-point for 'lein run' and uberjar."
