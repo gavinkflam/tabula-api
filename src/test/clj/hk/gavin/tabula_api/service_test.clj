f95d1ed8b8417c2403c1a60b5483ed46de5ecd95
Gavin Lam <me@gavin.hk>
2018-06-05 13:14:33 +0800

    Added file missing and non pdf tests

--- src/test/clj/hk/gavin/tabula_api/service_test.clj
+++ src/test/clj/hk/gavin/tabula_api/service_test.clj
@@ -5,19 +5,23 @@
             [hk.gavin.tabula-api.server :as server])
   (:import (org.apache.commons.io FileUtils)))
 
+(defn resource-file
+  [file-name]
+  (-> file-name io/resource io/file))
+
 (def base-req
   {:scheme :http
    :server-name "localhost"
    :server-port 8080
    :throw-exceptions false})
 
-(def sample-form
+(def base-form
   [{:name "area"   :content "%0,0,100,50"}
    {:name "area"   :content "%0,50,100,100"}
    {:name "format" :content "CSV"}
    {:name "pages"  :content "1"}
    {:name "stream" :content "yes"}
-   {:name "file"   :content (io/file (io/resource "multi-column.pdf"))}])
+   {:name "file"   :content (resource-file "multi-column.pdf")}])
 
 (defn request
   [req]
@@ -31,7 +35,7 @@
   (server/run-dev)
   (let [resp (request {:uri "/extract_tables"
                        :method :post
-                       :multipart sample-form})
+                       :multipart base-form})
         expect-csv (io/file (io/resource "multi-column.csv"))
         output-csv (java.io.File/createTempFile "extract-tables-test" ".csv")]
     (is (= (get resp :status) 200))
@@ -42,7 +46,24 @@
   (server/run-dev)
   (let [resp (request {:uri "/extract_tables"
                        :method :post
-                       :multipart (update-field sample-form "format" "FOO")})]
+                       :multipart (update-field base-form "format" "FOO")})]
     (is (= (get resp :status) 400))
     (is (= (get resp :body)
            "format FOO is illegal. Available formats: CSV,TSV,JSON"))))
+
+(deftest extract-tables-file-missing-error-test
+  (server/run-dev)
+  (let [resp (request {:uri "/extract_tables"
+                       :method :post
+                       :multipart (update-field base-form "file" "")})]
+    (is (= (get resp :status) 400))
+    (is (= (get resp :body) "file is missing."))))
+
+(deftest extract-tables-file-non-pdf-error-test
+  (server/run-dev)
+  (let [form (update-field base-form "file" (resource-file "multi-column.csv"))
+        resp (request {:uri "/extract_tables"
+                       :method :post
+                       :multipart form})]
+    (is (= (get resp :status) 400))
+    (is (= (get resp :body) "file is not a valid PDF file."))))
