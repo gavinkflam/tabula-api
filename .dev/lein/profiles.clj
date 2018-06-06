99f847f556b96ebbc82952f9aec1e5a7a8fac9b5
Gavin Lam <me@gavin.hk>
2018-06-06 18:03:58 +0800

    Added lein-cloverage to profiles for development environment

--- .dev/lein/profiles.clj
+++ .dev/lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.16.0"]
             [jonase/eastwood "0.2.6" :exclusions [org.clojure/clojure]]
+            [lein-cloverage "1.0.10" :exclusions [org.clojure/clojure]]
             [lein-cljfmt "0.5.7" :exclusions [org.clojure/clojure
                                               rewrite-cljs]]
             [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]]
