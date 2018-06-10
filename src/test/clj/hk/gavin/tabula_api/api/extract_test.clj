6b7fbfee806bb383a9103f4e901e0ddf61922237
Gavin Lam <me@gavin.hk>
2018-06-10 23:02:27 +0800

    Added Accept and Content-Type header tests

--- src/test/clj/hk/gavin/tabula_api/api/extract_test.clj
+++ src/test/clj/hk/gavin/tabula_api/api/extract_test.clj
@@ -7,7 +7,6 @@
 (def base-form
   [{:name "area"   :content "%0,0,100,50"}
    {:name "area"   :content "%0,50,100,100"}
-   {:name "format" :content "CSV"}
    {:name "pages"  :content "1"}
    {:name "stream" :content "yes"}
    {:name "file"   :content (util/resource-file "multi-column.pdf")}])
@@ -16,28 +15,30 @@
   (server/run-dev)
   (let [resp (util/request {:uri "/api/extract"
                             :method :post
+                            :accept "text/csv"
                             :multipart base-form})
         expect-csv (util/resource-file "multi-column.csv")
         output-csv (java.io.File/createTempFile "extract-tables-test" ".csv")]
     (is (= (get resp :status) 200))
+    (is (= (get-in resp [:headers :content-type]) "text/csv"))
     (FileUtils/writeStringToFile output-csv (get resp :body) "UTF-8")
     (is (FileUtils/contentEqualsIgnoreEOL expect-csv output-csv nil))))
 
 (deftest extract-tables-format-error-test
   (server/run-dev)
-  (let [form (util/update-field base-form "format" "FOO")
-        resp (util/request {:uri "/api/extract"
+  (let [resp (util/request {:uri "/api/extract"
                             :method :post
-                            :multipart form})]
+                            :accept "text/html"
+                            :multipart base-form})]
     (is (= (get resp :status) 400))
-    (is (= (get resp :body)
-           "format FOO is illegal. Available formats: CSV,TSV,JSON"))))
+    (is (= (get resp :body) "text/html is not supported."))))
 
 (deftest extract-tables-file-missing-error-test
   (server/run-dev)
   (let [form (util/update-field base-form "file" "")
         resp (util/request {:uri "/api/extract"
                             :method :post
+                            :accept "text/csv"
                             :multipart form})]
     (is (= (get resp :status) 400))
     (is (= (get resp :body) "file is missing."))))
@@ -48,6 +49,7 @@
               base-form "file" (util/resource-file "multi-column.csv"))
         resp (util/request {:uri "/api/extract"
                             :method :post
+                            :accept "text/csv"
                             :multipart form})]
     (is (= (get resp :status) 400))
     (is (= (get resp :body) "file is not a valid PDF file."))))
