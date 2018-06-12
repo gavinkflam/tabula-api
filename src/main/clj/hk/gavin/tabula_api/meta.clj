fc5bcd45300deecc6aabda57cb6df5d78e6438a1
Gavin Lam <me@gavin.hk>
2018-06-12 18:33:42 +0800

    Introduced env for flexible configurations

--- src/main/clj/hk/gavin/tabula_api/meta.clj
+++ src/main/clj/hk/gavin/tabula_api/meta.clj
@@ -1,5 +1,6 @@
 (ns hk.gavin.tabula-api.meta
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
   (:import (java.util Properties)))
 
 (def version-string
@@ -8,3 +9,23 @@
                  io/resource io/reader)
          props (doto (Properties.) (.load pom))]
      (.get props "version"))))
+
+; keywordize, java-map->map and env are derived from environ by James Reeves.
+; Distributed under Eclipse Public License.
+; https://github.com/weavejester/environ
+
+(defn- keywordize [s]
+  (-> s str/lower-case (str/replace "_" "-") (str/replace "." "-") keyword))
+
+(defn- java-map->map
+  [m]
+  (->> m (map (fn [[k v]] [(keywordize k) v])) (into {})))
+
+(def default-env
+  {:host "localhost"
+   :port "8080"})
+
+(defonce env
+  (merge default-env
+         (java-map->map (System/getenv))
+         (java-map->map (System/getProperties))))
--- src/main/clj/hk/gavin/tabula_api/service.clj
+++ src/main/clj/hk/gavin/tabula_api/service.clj
@@ -2,6 +2,7 @@
   (:require [clojure.set :as set]
             [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
+            [hk.gavin.tabula-api.meta :refer [env]]
             [hk.gavin.tabula-api.api.extract :as api-extract]
             [hk.gavin.tabula-api.api.version :as api-version]))
 
@@ -13,4 +14,5 @@
               ::http/routes        routes
               ::http/resource-path "/public"
               ::http/type          :immutant
-              ::http/port          8080})
+              ::http/host          (env :host)
+              ::http/port          (env :port)})
