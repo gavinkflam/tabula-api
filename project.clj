4c477c5a7983bd831f98e056a0312aed817a0ce3
Gavin Lam <me@gavin.hk>
2019-02-25 21:15:36 +0800

    Upgraded clojure and other dependencies

--- project.clj
+++ project.clj
@@ -5,29 +5,29 @@
             :url "https://opensource.org/licenses/MIT"
             :key "mit"
             :year 2018}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [commons-cli/commons-cli "1.4"]
-                 [io.pedestal/pedestal.service "0.5.4"]
-                 [io.pedestal/pedestal.immutant "0.5.4"]
-                 [org.apache.tika/tika-core "1.18"]
-                 [org.slf4j/slf4j-simple "1.7.25"]
+                 [io.pedestal/pedestal.service "0.5.5"]
+                 [io.pedestal/pedestal.immutant "0.5.5"]
+                 [org.apache.tika/tika-core "1.20"]
+                 [org.slf4j/slf4j-simple "1.7.26"]
                  [technology.tabula/tabula "1.0.2"]]
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
   :resource-paths ["src/main/resources"]
-  :profiles {:dev {:dependencies [[clj-http "3.9.0"]
+  :profiles {:dev {:dependencies [[clj-http "3.9.1"]
                                   [commons-io/commons-io "2.6"]]
-                   :plugins [[jonase/eastwood "0.2.6"
+                   :plugins [[jonase/eastwood "0.3.5"
                               :exclusions [org.clojure/clojure]]
                              [lein-bikeshed "0.5.1"]
-                             [lein-cloverage "1.0.10"
+                             [lein-cloverage "1.0.13"
                               :exclusions [org.clojure/clojure]]
-                             [lein-cljfmt "0.5.7"
+                             [lein-cljfmt "0.6.4"
                               :exclusions [org.clojure/clojure
                                            rewrite-cljs]]
                              [lein-kibit "0.1.6"
                               :exclusions [org.clojure/clojure]]
-                             [lein-nvd "0.5.3"]]
+                             [lein-nvd "0.6.0"]]
                    :resource-paths ["src/test/resources"]
                    ; Temporarily disable nvd failure until pedestal 0.5.4
                    :nvd {:fail-threshold 11}}}
